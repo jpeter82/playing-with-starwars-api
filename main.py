fb713c95df6427b17bb31c3960c74fe093e4dc00
Peter Juhasz <jpeter82@gmail.com>
2017-07-02 20:30:33 +0200

    started working on main.py, created basic routes

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
+
+from flask import Flask, render_template, url_for, session, redirect, request, jsonify
+import datetime
+import settings
+import users
+import votes
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 
 @app.route('/')
 def index():
-    return render_template('planets.html')
+    return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+@app.route('/content')
+@users.login_required
+def content():
+    return render_template('content.html')
