0f5d276a9855f9c0da08c7d1e031fc280ef1f5ed
Peter Juhasz <jpeter82@gmail.com>
2017-07-02 20:35:56 +0200

    added routes for error handling

--- main.py
+++ main.py
@@ -92,3 +92,16 @@ def vote():
 @users.login_required
 def get_vote_statistics():
     return jsonify({'vote_stats': votes.get_votes()})
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
+
+if __name__ == '__main__':
+    app.run(debug=settings.DEV_STATUS)
